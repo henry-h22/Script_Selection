756c209b8db609aedeff6de250d099df9d5a843b
Henry Heyden <henryden22@gmail.com>
2025-03-14 13:36:55 +0000

    undo changes done only for convergence exploration

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ randUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'random', e
 
 utteranceListList = [propUtts, linUtts, onesUtts, randUtts]
 costFunctionsList = ['Proportional', 'Linear', 'Ones', 'Random']
-for i in range(4):
-    print('Counting function {} collected every possible diphone in {} utterances'.format(costFunctionsList[i], len(utteranceListList[i])))
-
-
 
 # print(evaluateUtterance(randUtts, udcm.copy(), overallDiphoneCounts.copy()))
-# visuallyEvaluateUtterances(utteranceListList, udcm.copy(), overallDiphoneCounts.copy(), ['Proportional', 'Linear', 'Ones', 'Random'])
+visuallyEvaluateUtterances(utteranceListList, udcm.copy(), overallDiphoneCounts.copy(), costFunctionsList)
