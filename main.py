1f7fe69a468a8f5d07257f9c5174db616835c1e7
Henry Heyden <henryden22@gmail.com>
2025-03-14 12:44:17 +0000

    updates to main

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from readIn import readFile
 from scriptSelection import scriptSelection
 from evaluation import evaluateUtterance
+from evaluation import visuallyEvaluateUtterances
 
 udcm, overallDiphoneCounts = readFile('simonH.txt')
 
-setUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'set')
-multUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'mult')
-awareUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'aware')
+setUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'set', endConditionParameter=400)
+multUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'mult', endConditionParameter=400)
+awareUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'aware', endConditionParameter=400)
+randUtts = scriptSelection(udcm.copy(), overallDiphoneCounts.copy(), 'random', endConditionParameter=400)
 
-print(evaluateUtterance(setUtts, udcm.copy(), overallDiphoneCounts.copy()))
+utteranceListList = [setUtts, multUtts, awareUtts, randUtts]
+
+# print(evaluateUtterance(randUtts, udcm.copy(), overallDiphoneCounts.copy()))
+visuallyEvaluateUtterances(utteranceListList, udcm.copy(), overallDiphoneCounts.copy(), ['Set', 'Multiple', 'Aware', 'Random'])
