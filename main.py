6139096633b9bec59eeb4a68c3a0e36260b48c73
Henry Heyden <henryden22@gmail.com>
2025-03-14 19:34:39 +0000

    ""

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from readIn import readFile
 from scriptSelection import scriptSelection
-from evaluation import evaluateUtterance
-from evaluation import visuallyEvaluateUtterances
+# from evaluationScoringFunctions import evaluateUtteranceScoring
+from evaluationScoringFunctions import visuallyEvaluateUtterancesScoring
 
 udcm, overallDiphoneCounts = readFile('simonH.txt')
 
@@ -16,4 +16,4 @@ utteranceListList = [propUtts, linUtts, onesUtts, randUtts]
 costFunctionsList = ['Proportional', 'Linear', 'Ones', 'Random']
 
 # print(evaluateUtterance(randUtts, udcm.copy(), overallDiphoneCounts.copy()))
-visuallyEvaluateUtterances(utteranceListList, udcm.copy(), overallDiphoneCounts.copy(), costFunctionsList, verbose=True)
+visuallyEvaluateUtterancesScoring(utteranceListList, udcm.copy(), overallDiphoneCounts.copy(), costFunctionsList, verbose=True)
