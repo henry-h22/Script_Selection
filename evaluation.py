cc3d6d04615b965f4a391c71f61b3293c80b0755
Henry Heyden <henryden22@gmail.com>
2025-03-14 12:39:22 +0000

    Pretty up the visual evaluation graphs

--- evaluation.py
+++ evaluation.py
@@ -1,45 +1,52 @@
 import matplotlib.pyplot as plt
 from helperFunctions import dictionaryIncrement as dI
 
-def evaluateUtterance(utteranceList, udcm, overallDiphoneCounts, subplotThing = None):
+def evaluateUtterance(utteranceList, udcm, overallDiphoneCounts, subplotThing = None, scriptSelectionAlgorithm = '__', verbose = False):
 
-    # Create our diphone set, for later
-    diphoneSet = set(overallDiphoneCounts.copy().keys())
+    # Create our variables
+    totalDiphoneSet = set(overallDiphoneCounts.copy().keys())
+    selectedDiphonesSet = set([])
+    rawDiphonesList = []
 
-    # Create our diphones -> counts map for this SPECIFIC list of utterances
-    utterranceDiphoneCounts = {}
-
-    # Then we wanna create the data that we want to graph, starting with a diphone->count map over the whole list
-    # As we do so, we keep track of number of diphones that it could've grabbed but didn't, because that's also a metric!
+    # Then we wanna create the data that we want to graph, which is a list of every diphone in the whole utterance list
+    # As we do so, we keep track of number of diphone types that it could've grabbed but didn't, because that's also a metric!
     for utterance in utteranceList:
         for diphone in udcm[utterance].keys():
+            selectedDiphonesSet.add(diphone) # we add the diphone to the set of selected units
             if diphone == 'total': continue
-            dI(utterranceDiphoneCounts, diphone, udcm[utterance][diphone])
-    
+            for _ in range(udcm[utterance][diphone]):
+                rawDiphonesList.append(hash(diphone)) # we append the hashcode here because pyplot's ecdf function can't take strings
 
-    rawDiphonesList = []
-    for diphone in utterranceDiphoneCounts.keys():
-        for _ in range(utterranceDiphoneCounts[diphone]):
-            rawDiphonesList.append(hash(diphone))
+    missedDiphones = totalDiphoneSet - selectedDiphonesSet
+
+    
+    print('Script selection algorithm {} failed to select {} of {} possible diphones.'.format(scriptSelectionAlgorithm, len(missedDiphones), len(totalDiphoneSet)-1))
+    if verbose:
+        print('Those diphones are:\n')
+        print(missedDiphones)
 
     if subplotThing is not None:
+        # in this case, we called the function from visuallyEvaluateUtterances, and we want to do something very specific
+        subplotThing.set_title(scriptSelectionAlgorithm)
         subplotThing.ecdf(rawDiphonesList)
     else:
+        # here we just wanna plot it :)
         fig, ax = plt.subplots()
-        # ax.hist(rawDiphonesList, bins = len(utterranceDiphoneCounts.keys()), linewidth = 0.022)
+        # ax.hist(rawDiphonesList, bins = len(utteranceDiphoneCounts.keys()), linewidth = 0.022)
         ax.ecdf(rawDiphonesList)
         plt.show()
 
-    # return utterranceDiphoneCounts
+    # return utteranceDiphoneCounts
 
 
-def visuallyEvaluateUtterances(utteranceListList, udcm, overallDiphoneCounts):
+def visuallyEvaluateUtterances(utteranceListList, udcm, overallDiphoneCounts, selectionAlgorithms, verbose = False):
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, sharex=True, sharey=True)
     
-    evaluateUtterance(utteranceListList[0], udcm.copy(), overallDiphoneCounts.copy(), ax1)
-    evaluateUtterance(utteranceListList[1], udcm.copy(), overallDiphoneCounts.copy(), ax2)
-    evaluateUtterance(utteranceListList[2], udcm.copy(), overallDiphoneCounts.copy(), ax3)
-    evaluateUtterance(utteranceListList[3], udcm.copy(), overallDiphoneCounts.copy(), ax4)
+    fig.suptitle('Diphone Distribution over Script Selection Algorithms')
+
+    evaluateUtterance(utteranceListList[0], udcm.copy(), overallDiphoneCounts.copy(), ax1, selectionAlgorithms[0], verbose)
+    evaluateUtterance(utteranceListList[1], udcm.copy(), overallDiphoneCounts.copy(), ax2, selectionAlgorithms[1], verbose)
+    evaluateUtterance(utteranceListList[2], udcm.copy(), overallDiphoneCounts.copy(), ax3, selectionAlgorithms[2], verbose)
+    evaluateUtterance(utteranceListList[3], udcm.copy(), overallDiphoneCounts.copy(), ax4, selectionAlgorithms[3], verbose)
 
     plt.show()
-    
