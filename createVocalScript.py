3cffc7b2ef398315ad9d227744f0121b63e2bf9c
Henry Heyden <henryden22@gmail.com>
2025-03-21 11:44:39 +0000

    Implement script selection for voice creation

---
+++ createVocalScript.py
@@ -0,0 +1,45 @@
+# THIS FILE IS A COPY OF exportScript made specifically to collect the script for PARTICULAR voices
+# A task that, honestly, I thought was gonna be easier. Oops.
+
+from readIn import readFile
+from scriptSelection import scriptSelectionHybrid as hssh
+
+def readUttDataLines(filename = "simonHutts.data"):
+    phil = open(filename)
+    lines = []
+    for line in phil:
+        lines.append(line)
+    return lines
+
+
+def createIndividualVoiceDatabase(mlfFile = 'allUTTSRenumbered.mlf', diphoneThreshold = 222222, outputFilename = 'newnewnewutts.data'):
+    udcm, overallDiphoneCounts = readFile(mlfFile, scriptNameLength = 6)
+    # print(udcm)
+
+    numberOfUtterances = len(udcm.keys()) #! we want EVERY utterance chosen, thanks
+
+    # Perform Hybrid: Set script selection with the Ones cost function, cuz of the result of the experiments
+    chosenUtterances = hssh(udcm.copy(), overallDiphoneCounts.copy(), 
+                            selectionFunction = 'set', scoringFunction = 'ones', endConditionParameter=numberOfUtterances)
+
+    utteranceDataLines = readUttDataLines("allUTTS.data") #! note that this is NOT the renumbered one. we dont wanna confuse festival!
+
+    # What's interesting about this is that we want to choose a certain number of diphones, not utterances. So....
+    diphonesChosen = 0
+    newDataLines = []
+
+    for i, utterance in enumerate(chosenUtterances):
+        if diphonesChosen > diphoneThreshold: 
+            print('Threshold reached!', diphonesChosen)
+            break
+        diphonesChosen += udcm[utterance]['total']
+        newLine = utteranceDataLines[int(utterance) - 1]
+        newDataLines.append(newLine) # note that we don't renumber. IMPORTANT destinction from exportScript.py
+
+    print(f'We chose {diphonesChosen} diphones!!')
+    newFile = open(outputFilename, "w")
+    newFile.writelines(newDataLines)
+    newFile.close()
+
+if __name__ == '__main__':
+    createIndividualVoiceDatabase()
